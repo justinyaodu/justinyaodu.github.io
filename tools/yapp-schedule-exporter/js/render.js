2f02e77d4d2fd2786dd5a1700a711104806bc5d6
Justin Yao Du <justinyaodu@gmail.com>
2020-09-23 23:38:11 -0700

    Update Yapp schedule exporter

--- tools/yapp-schedule-exporter/js/render.js
+++ tools/yapp-schedule-exporter/js/render.js
@@ -3,7 +3,7 @@
 /**
  * Display the schedule data and return whether anything was rendered.
  */
-function render(instancesOf) {
+function renderSchedule(instancesOf) {
   let contentsRendered = false;
   instancesOf.ScheduleTrack.sort(ScheduleTrack.compareBySortOrder);
   for (const track of instancesOf.ScheduleTrack) {
@@ -42,8 +42,9 @@ function renderTrackHeading(track) {
 
   const id = trackId(track);
 
-  const collapseButton = makeCollapseButton(trackId(track), false);
-  collapseButton.classList.add("ml-3");
+  const collapseButton = makeCollapseButton(id, false);
+  // Align with collapse buttons on event groups.
+  collapseButton.style = "margin-left: calc(0.75rem + 1px);";
   div.appendChild(collapseButton);
 
   const heading = document.createElement("h2");
